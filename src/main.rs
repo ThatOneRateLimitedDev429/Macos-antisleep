a0a7f8e79eb4a043847457dd36039caf0769cdba
DragonShadow9 <276484@gcloud.tisd.org>
2024-06-08 04:26:00 -0500

    update 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,57 @@ use colored::Colorize;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        // TODO: make this help better
-        println!("Use antisleep 1 for sleep on | 0 for sleep off. \n use help to see this msg.")
+        help();
+        return;
     } else {
-        if args[1] == "1" {
-            println!("{}","this will ask for a password.".yellow());
-            Command::new("sh")
-                .arg("-c")
-                .arg("sudo pmset -b sleep 1 && sudo pmset -b disablesleep 0")
-                .output()
-                .expect("failed to execute process");
-            println!("{}","The sleep functionality has been enabled!".green())
-        } else if args[1] == "0" {
-            println!("{}","A password will be requested.".yellow());
-            Command::new("sh")
-                .arg("-c")
-                .arg("sudo pmset -b sleep 0 && sudo pmset -b disablesleep 1")
-                .output()
-                .expect("failed to execute process");
-            println!("{}","sleep has been disabled".green())
+        if args[1] == "-h" || args[1] == "help"{
+           help();
+           return;
+        } else if args[1] == "-v" || args[1] == "version" {
+            version();
+            return;
+        } else if args[1] == "off" || args[1] == "0" {
+            sleep_off();
+            return;
+        } else if args[1] == "on" || args[1] == "1" {
+            sleep_on();
+            return;
+        } else {
+            println!("{}","Arg not found...".red());
+            help();
+            std::process::exit(1);
         }
+
+
 
     }
 
 
 }
+fn help() {
+    println!("{}","-h, help : shows this help screen \n-v, version : shows the version of antisleep \n1, on : turns your computer's sleep functionality on \n0, off : turns the sleep functionality off".bright_yellow())
+}
+
+fn sleep_on() {
+    Command::new("sh")
+        .arg("-c")
+        .arg("sudo pmset -b sleep 1 && sudo pmset -b disablesleep 0")
+        .output()
+        .expect("failed to run sleep on commands..\n run sudo pmset -b sleep 1 and sudo pmset -b disablesleep 0 in terminal");
+    println!("{}","The sleep functionality has been enabled.".green())
+    }
+
+
+fn sleep_off() {
+    Command::new("sh")
+        .arg("-c")
+        .arg("sudo pmset -b sleep 0 && sudo pmset -b disablesleep 1")
+        .output()
+        .expect("failed to run sleep on commands..\n run sudo pmset -b sleep 0 and sudo pmset -b disablesleep 1 in terminal");
+    println!("{}","The sleep functionality has been disabled.".green())
+}
+
+fn version() {
+    println!("{}","0.1.1".green())
+}
+
